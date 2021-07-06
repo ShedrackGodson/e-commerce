047b6015a06e3975355c437dbe0af156f305be3c
ShedrackGodson <hedrickgodson23@gmail.com>
2021-07-06 14:11:20 +0300

    now fetch dummy from the backend

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,31 @@
-import React from "react";
-import products from "../products";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import axios from "axios";
 
 function HomeScreen() {
-  return (
-    <div className="py-4">
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product prod={product /** Passing a prop */}/> 
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		async function fetchProducts() {
+			const { data } = await axios.get("http://localhost:8000/api/products/");
+      setProducts(data);
+		}
+
+		fetchProducts();
+	}, []);
+	return (
+		<div className="py-4">
+			<h1>Latest Products</h1>
+			<Row>
+				{products.map((product) => (
+					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+						<Product prod={product /** Passing a prop */} />
+					</Col>
+				))}
+			</Row>
+		</div>
+	);
 }
 
 export default HomeScreen;
